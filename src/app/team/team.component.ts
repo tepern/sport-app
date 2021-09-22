7d3012c7dc93deb88e4af424584941974872b524
Natalia Teper <teper.n@gmail.com>
2021-09-22 15:11:48 +0400

    Inctance text search

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Team } from '../team';
 import { HttpService } from '../http.service';
 import { Subscription } from 'rxjs';
+import { NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-team',
@@ -10,17 +11,28 @@ import { Subscription } from 'rxjs';
 })
 export class TeamComponent implements OnInit {
 
-  teams: Team[] = [];
-  search: string = '';
+    teams: Team[] = [];
+    textSearch: string = '';
 
-  constructor(private httpService: HttpService) {
+    constructor(private httpService: HttpService) {
 
-  }
+    }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
       this.httpService.getTeam().subscribe((data: Team[]) => {console.log(data); this.teams=data;});
+    }
 
- 
-  }
+    onSearchChange(): void {
+        if(this.textSearch) {
+            const teams = this.teams;
+            const search = this.textSearch;
+            console.log(this.textSearch);
+            this.teams = teams.filter(function(team) {
+                return team.name.indexOf(search)>-1;
+            })
+        } else {
+            this.httpService.getTeam().subscribe((data: Team[]) => {console.log(data); this.teams=data;});
+        }
+    }
 
 }
